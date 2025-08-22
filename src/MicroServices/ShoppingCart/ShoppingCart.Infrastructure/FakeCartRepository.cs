c6119e3af3fd83cf80ea088d3b8c1949493399fb
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-08-22 11:20:31 +0200

    Dodanie obsługi koszyka w Redis

--- src/MicroServices/ShoppingCart/ShoppingCart.Infrastructure/FakeCartRepository.cs
+++ src/MicroServices/ShoppingCart/ShoppingCart.Infrastructure/FakeCartRepository.cs
@@ -8,4 +8,9 @@ public class FakeCartRepository : ICartRepository
     public void Add(string sessionId, Product product)
     {
     }
+
+    public void Delete(string sessionId, int productId)
+    {
+        throw new NotImplementedException();
+    }
 }
---
+++ src/MicroServices/ShoppingCart/ShoppingCart.Infrastructure/RedisCartRepository.cs
@@ -0,0 +1,33 @@
+﻿using Shared.Domain.Entities;
+using ShoppingCart.Domain.Abstractions;
+using StackExchange.Redis;
+
+namespace ShoppingCart.Infrastructure;
+
+// dotnet add package StackExchange.Redis
+public class RedisCartRepository(IConnectionMultiplexer connection) : ICartRepository
+{
+    private readonly IDatabase db = connection.GetDatabase(0);
+
+    public void Add(string sessionId, Product product)
+    {
+        string key = $"cart:{sessionId}";
+        string field = $"product:{product.Id}";
+
+        db.HashIncrement(key, field); // HINCRBY key field 1
+        db.KeyExpire(key, TimeSpan.FromMinutes(3)); // EXPIRE key time
+    }
+
+    public void Delete(string sessionId, int productId)
+    {
+        string key = $"cart:{sessionId}";
+        string field = $"product:{productId}";
+
+        var quantity = db.HashDecrement(key, field);
+
+        if (quantity == 0)
+            db.HashDelete(key, field);
+
+        db.KeyExpire(key, TimeSpan.FromMinutes(3)); // EXPIRE key time
+    }
+}
