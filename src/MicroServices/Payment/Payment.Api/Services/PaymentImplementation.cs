253f7759c4ff82ac84452d45b75c85eac643351c
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-09-01 11:46:09 +0200

    Dodanie proto

---
+++ src/MicroServices/Payment/Payment.Api/Services/PaymentImplementation.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Core;
+using PaymentService.Grcp;
+
+namespace Payment.Api.Services;
+
+// {csharp_namespace}.{service}.{service}Base
+public class PaymentImplementation : PaymentService.Grcp.Payment.PaymentBase
+{
+    public override async Task<PaymentResponse> AuthorizePayment(PaymentRequest request, ServerCallContext context)
+    {
+        // mock: 80% zaakceptowane, 20% odrzucone
+        if (Random.Shared.NextDouble() < 0.2)
+        {
+            return new PaymentResponse { Status = PaymentStatus.Declined, Reason = "Brak środków na koncie" };
+        }
+
+        return new PaymentResponse { Status = PaymentStatus.Accepted };
+    }
+  
+}
